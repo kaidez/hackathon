75849da83db48ab2ee3680d5c04e9659c4e95f4f
Kai <kl110865@users.noreply.github.com>
2018-06-28 12:48:34 -0400

    Added routes and route components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,16 @@ import './App.css';
 import Grid from './listings/Grid';
 import courses from '../data/courses.json';
 import Header from './Header';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Register from './Register';
+import SignUp from './SignUp';
+import Update from './Update';
+import Create from './Create';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {courses};
   }
 
   render() {
@@ -18,9 +23,15 @@ export default class App extends Component {
           <nav className="nav-header">
             <Header />
           </nav>
-        </div>
-        <div>
-            <Grid items={this.state.courses}/>
+          <Router>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/update" component={Update} />
+              <Route exact path="/create" component={Create} />
+            </div>
+          </Router>
         </div>
       </div>
     );
---
+++ src/components/Update.jsx
@@ -0,0 +1,19 @@
+// @flow
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+export default class Update extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+          <h1>Update</h1>
+      </div>
+    );
+  }
+}
