560b232ce23687c5e9002b32d00e5ba7a5ce3987
Zihe Xia <zihex@umich.edu>
2018-06-28 11:20:12 -0400

    Header.jsx added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Grid from './listings/Grid';
 import courses from '../data/courses.json';
+import Header from './Header';
 
 export default class App extends Component {
   constructor(props) {
@@ -15,23 +16,7 @@ export default class App extends Component {
       <div>
         <div className="navbar-fixed">
           <nav className="nav-header">
-            <div className="nav-wrapper">
-              <div>
-                <h1 href="#" className="brand-logo center">Hackathon</h1>
-              </div>
-              {/* <ul id="nav-mobile" className="left hide-on-med-and-down">
-                <li><a href="#">React</a></li>
-                <li><a href="#">React Native</a></li>
-                <li><a href="#">Framer</a></li>
-              </ul> */}
-              <ul className="main-menu">
-                <li>Register for Event</li>
-                <li>Create / Update</li>
-                <li>Sign up for Project</li>
-                <li>Location Preparation</li>
-                <li>Update Project Post-Hackathon</li>
-              </ul>
-            </div>
+            <Header />
           </nav>
         </div>
         <div>
